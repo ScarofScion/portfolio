467e661a0e29e54f9d35d87c4c01b5addab5f3ac
scarofscion <scarofscion@gmail.com>
2019-02-21 11:14:11 -0800

    Updates

---
+++ precache-manifest.b10296388404a4fdbcd6e8a7dff79809.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "e42378083df23c83ddc4",
+    "url": "/portfolio/static/js/main.e4237808.chunk.js"
+  },
+  {
+    "revision": "8002abff99c5d3f9c8f2",
+    "url": "/portfolio/static/js/2.8002abff.chunk.js"
+  },
+  {
+    "revision": "3d6e8f43f305d0b745e93b07ab9e563e",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.a58d0115337e5f3397e2cf0ac446a8c3.js"
+  "/portfolio/precache-manifest.b10296388404a4fdbcd6e8a7dff79809.js"
 );
 
 workbox.clientsClaim();
